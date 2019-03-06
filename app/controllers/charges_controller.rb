8f9d59bfa0126e787d87273ea77a6ec83b2dcef0
Octopussyzz <spudd.otw@gmail.com>
2019-03-06 20:22:20 +0100

    WiP on stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,17 +1,16 @@
 class ChargesController < ApplicationController
   include BasketsHelper
   before_action :set_price
+  before_action :set_amount
 
   def new
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create
     # Amount in cents
-    puts "$" * 100
-    puts @price
-    puts "$" * 100
-    @amount = @price * 100
-
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
       source: params[:stripeToken],
@@ -27,6 +26,12 @@ class ChargesController < ApplicationController
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
+  end
+
+  private
+
+  def set_amount
+    @amount = (@price * 100).to_i
   end
 
 end
