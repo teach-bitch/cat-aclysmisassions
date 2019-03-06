447b9ce8037ed72f4b5ac500e16203342c2a89ca
TheWalkingNev <alexandre.gasior@gmail.com>
2019-03-06 01:09:14 +0100

    most of the basket is done, need fix for add/remove quantity

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,76 +1,49 @@
 class LineItemsController < ApplicationController
   include CurrentBasket
   before_action :set_basket, only: [:create]
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_line_item, only: [:new, :update, :destroy]
 
-  # GET /line_items
-  # GET /line_items.json
-  def index
-    @line_items = LineItem.all
-  end
-
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-  end
-
-  # GET /line_items/new
-  def new
-    @line_item = LineItem.new
-  end
-
-  # GET /line_items/1/edit
-  def edit
-  end
-
-  # POST /line_items
-  # POST /line_items.json
-  def create
+  def create  # add 1 product to basket
     product = Product.find(params[:product_id])
     @line_item = @basket.add_product(product)
-
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.basket, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to @line_item.basket, notice: 'Produit ajouté au panier.' }
       else
         format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+  def update  # add or remove 1 product from basket
+    if
+      @line_item.quantity += 1
+      @line_item.save
+    else
+      if @line_item.quantity - 1 == 0 # if quantity will be equal to 0 then destroy
+        @line_item.destroy
       else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        @line_item.quantity -= 1  # else only subtract 1 to quantity
+        @line_item.save
       end
+    end
+    respond_to do |format|
+      format.html { redirect_to @line_item.basket, notice: 'Panier mis à jour.' }
     end
   end
 
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
-  def destroy
+  def destroy # destroy completely one type of product from the basket
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to @line_item.basket, notice: 'Produit supprimé du panier.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_line_item
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
       params.require(:line_item).permit(:basket_id, :product_id, :quantity)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root "products#index"
-  get '/home/index'	
+  get '/home/index'
   devise_for :users
 
-  resources :line_items, only: [:create, :edit, :update, :destroy, :new]
-  resources :facturations, except: [:destroy ,:new]
-  resources :baskets, except: [:index ,:new]
-  resources :categories
-  resources :products
+  resources :line_items, only: [:create, :update, :destroy]
+  resources :facturations, only: [:index, :show, :create]
+  resources :baskets, only: [:show, :create, :destroy]
+  resources :categories, except: [:new, :edit]
+  resources :products, except: [:new, :edit]
 end
